65b14d6b8a7b70354dbaad6d4c18bcc8a7acd8e5
gunjan patel <g.a.patel359@gmail.com>
2024-10-09 20:21:47 +0530

    work

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const backend_url="https://food-app-backend-gamma.vercel.app/api/v1"
-export const img_url="https://food-app-backend-gamma.vercel.app"
+export const backend_url="https://food-app-backend-green.vercel.app/api/v1"
+export const img_url="https://food-app-backend-green.vercel.app"
 
 export const theme_colors=["blue","rose"]
