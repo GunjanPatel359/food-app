82f4e26933bad11cb68ce9282f23dcca62216543
gunjan patel <g.a.patel359@gmail.com>
2024-10-10 18:13:27 +0530

    work

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,8 +2,8 @@ const sendToken =async(user,statusCode,res,msg,rest)=>{
     const token=await user.getJwtToken(user._id);
     const options={
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-        httpOnly:true,
-        secure: process.env.NODE_ENV === 'PRODUCTION',
+        httpOnly: true,
+        secure: true,
         sameSite: 'none'   
     };
     res.status(statusCode).cookie("token",token,options).json({
