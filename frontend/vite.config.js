633baa7d3f9ab857332e242b4333346a5f0b683a
gunjan patel <g.a.patel359@gmail.com>
2024-10-10 19:21:29 +0530

    work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: '/index.html',
+      input: '/index.html', 
     },
-  },
-  server: {
-    historyApiFallback: true,
   },
 });
