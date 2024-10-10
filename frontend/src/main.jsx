0835352eb95bbfb11ab660827e54a28c34e1b6c7
gunjan patel <g.a.patel359@gmail.com>
2024-10-10 19:11:30 +0530

    work

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,10 +8,10 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+//  <GoogleOAuthProvider clientId='619764962057-eauvmt3i6nnjoqhb6fh0elsk6jhal94e.apps.googleusercontent.com' onScriptLoadError={()=>console.log("hi")}>
+//   </GoogleOAuthProvider> 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-  // <GoogleOAuthProvider clientId='619764962057-eauvmt3i6nnjoqhb6fh0elsk6jhal94e.apps.googleusercontent.com' onScriptLoadError={()=>console.log("hi")}>
-  //   </GoogleOAuthProvider>
   {/* <React.StrictMode> */}
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
